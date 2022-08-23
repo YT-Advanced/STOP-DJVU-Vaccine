d5983907f90eae46debab650d98f48988570bc76
YT Advanced <70064328+YT-Advanced@users.noreply.github.com>
2022-08-23 12:40:33 +0700

    Update vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -2,9 +2,6 @@
 # Prevents STOP/DJVU Ransomware from encrypting your files
 # Note: This may not work for future versions of this ransomware
 # So backup your files!
-#
-# Authors: Karsten Hahn, John Parol @ GDATA CyberDefense
-# Twitter: @struppigel
 
 import ctypes
 import win32con, win32api
